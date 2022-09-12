11320067eb37e9fff711c49b95e99bcd79312074
Nuno Coração <nunocoracao@gmail.com>
2022-09-12 12:49:52 +0100

    added firebase support and views to theme

---
+++ assets/js/process.js
@@ -0,0 +1,43 @@
+mediumZoom(document.querySelectorAll("img:not(.nozoom)"), {
+    margin: 24,
+    background: 'rgba(0,0,0,0.5)',
+    scrollOffset: 0,
+})
+
+if (typeof auth !== 'undefined') {
+    var viewsCollection = db.collection('views');
+
+    function numberWithCommas(x) {
+        //return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    }
+
+    var update_views = function (oid, id) {
+        viewsCollection.doc(id).onSnapshot(doc => {
+            var data = doc.data();
+            if (data) {
+                var label = document.querySelectorAll("span[id='" + oid + "']")[0].innerText.split(' ')[1]
+                document.querySelectorAll("span[id='" + oid + "']")[0].innerText = numberWithCommas(data.views) + " " + label
+            }
+        })
+    }
+
+
+    auth.signInAnonymously()
+        .then(() => {
+            var views_nodes = document.querySelectorAll("span[id^='views_']")
+
+            for (var i in views_nodes) {
+                var oid = views_nodes[i].id
+                var id = oid ? oid.replaceAll("/", "-") : oid
+                if (id) {
+                    update_views(oid, id)
+                }
+            }
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.error(errorCode, errorMessage)
+        });
+}
