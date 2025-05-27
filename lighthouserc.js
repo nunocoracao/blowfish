fb2c7b88e2fcba6a504cfcfa137f990e7ad3c8b3
ZhenShuo Leo <98386542+ZhenShuo2021@users.noreply.github.com>
2025-05-28 01:12:23 +0800

    chore: adjust develope command

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      startServerCommand: "npm run example -- --disableLiveReload --minify",
+      startServerCommand: "npm run example:production -- --disableLiveReload --minify",
       startServerReadyPattern: "Web Server is available",
       url: ["http://localhost:8008/blowfish/samples/emoji/"],
       settings: {
