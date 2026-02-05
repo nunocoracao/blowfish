f6aab85b7354532b1cb39caf0ab4f409f410533e
ZhenShuo Leo <98386542+ZhenShuo2021@users.noreply.github.com>
2026-02-05 19:18:17 +0800

    fix: CSS specificity and variable issues

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,13 +1,13 @@
 function css(name) {
-  return "rgb(" + getComputedStyle(document.documentElement).getPropertyValue(name) + ")";
+  return getComputedStyle(document.documentElement).getPropertyValue(name);
 }
 
 Chart.defaults.font.size = 14;
-Chart.defaults.backgroundColor = css("--color-primary-300");
-Chart.defaults.elements.point.borderColor = css("--color-primary-400");
-Chart.defaults.elements.bar.borderColor = css("--color-primary-500");
+Chart.defaults.backgroundColor = css("--clr-primary-300");
+Chart.defaults.elements.point.borderColor = css("--clr-primary-400");
+Chart.defaults.elements.bar.borderColor = css("--clr-primary-500");
 Chart.defaults.elements.bar.borderWidth = 1;
-Chart.defaults.elements.line.borderColor = css("--color-primary-400");
-Chart.defaults.elements.arc.backgroundColor = css("--color-primary-200");
-Chart.defaults.elements.arc.borderColor = css("--color-primary-500");
+Chart.defaults.elements.line.borderColor = css("--clr-primary-400");
+Chart.defaults.elements.arc.backgroundColor = css("--clr-primary-200");
+Chart.defaults.elements.arc.borderColor = css("--clr-primary-500");
 Chart.defaults.elements.arc.borderWidth = 1;
